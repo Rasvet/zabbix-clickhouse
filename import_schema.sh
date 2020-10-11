be9c3b5d43d42044491847935759b2c7ea7f06ae
Artem <Artemxp@gmail.com>
2020-10-11 22:05:34 +1000

    add transport for mysql

--- import_schema.sh
+++ import_schema.sh
@@ -4,5 +4,5 @@ set -a
 [ -f .env ] && . .env
 set +a
 
-cat schema.sql | docker exec -it mysql-server-80 mysql -uroot -p${MYSQL_ROOT_PASSWORD}
+cat schema.sql | docker exec -it mysql-server-80 mysql --host=localhost --protocol=TCP -uroot -p${MYSQL_ROOT_PASSWORD}
 
