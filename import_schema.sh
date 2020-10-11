6abc12bf966a897d2ac0ab7ac64a037154098e7a
Artem Khudoley <artemxp@gmail.com>
2020-10-11 22:43:26 +1000

    add root user mysql

--- import_schema.sh
+++ import_schema.sh
@@ -4,5 +4,8 @@ set -a
 [ -f .env ] && . .env
 set +a
 
-cat schema.sql | docker exec -it mysql-server-80 mysql --host=localhost --protocol=TCP -uroot -p${MYSQL_ROOT_PASSWORD}
+#CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+#GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 
+mysql --host=${MYSQL_HOST} -P${MYSQL_PORT} --protocol=TCP -uroot -p${MYSQL_ROOT_PASSWORD} -fe "CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED WITH mysql_native_password BY '${MYSQL_ROOT_PASSWORD}';GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
+cat schema.sql | mysql --host=${MYSQL_HOST} -P${MYSQL_PORT} --protocol=TCP -uroot -p${MYSQL_ROOT_PASSWORD} -f
--- schema.sql
+++ schema.sql
@@ -1,5 +1,7 @@
-
+SET GLOBAL validate_password.policy=LOW;
+set global validate_password.length=4;
 # Used by monitoring module for connection health
-CREATE USER IF NOT EXISTS monitor@'%' IDENTIFIED BY '"ProxysqlMonitor#';
+CREATE USER IF NOT EXISTS monitor@'%' IDENTIFIED WITH mysql_native_password BY 'ProxysqlMonitor#';
 GRANT ALL PRIVILEGES ON meta.* TO 'monitor'@'%';
 FLUSH PRIVILEGES;
+show global variables like '%password%';
