c473fa975c8e05572de135a1cbf6b5bcde231a8c
Artem Khudoley <artemxp@gmail.com>
2020-10-12 00:01:17 +1000

    change network mode

--- schema.sql
+++ schema.sql
@@ -1,7 +1,8 @@
-SET GLOBAL validate_password.policy=LOW;
-set global validate_password.length=4;
+#SET GLOBAL validate_password.policy=LOW;
+#set global validate_password.length=4;
 # Used by monitoring module for connection health
+DROP USER IF EXISTS  monitor@'%';
 CREATE USER IF NOT EXISTS monitor@'%' IDENTIFIED WITH mysql_native_password BY 'ProxysqlMonitor#';
 GRANT ALL PRIVILEGES ON meta.* TO 'monitor'@'%';
+GRANT ALL PRIVILEGES ON zabbix.* TO 'zabbix'@'%';
 FLUSH PRIVILEGES;
-show global variables like '%password%';
